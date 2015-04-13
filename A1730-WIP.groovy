2301363e53c03c03cd7909a9bb9db117b679f869
armandr <Armand.raz@gmail.com>
2015-04-13 16:26:14 -0400

    Basic Working functions With Icons

--- A1730-WIP.groovy
+++ A1730-WIP.groovy
@@ -14,6 +14,8 @@ metadata {
 		capability "Refresh"
 		capability "Sensor"
 
+    attribute "modeStatus", "string"
+
 				fingerprint profileId: "0104", inClusters: "0000,0003,0004,0005,0201,0204,0B05", outClusters: "000A, 0019"
 
 	}
@@ -25,26 +27,42 @@ metadata {
 		valueTile("temperature", "device.temperature", width: 2, height: 2) {
 			state("temperature", label:'${currentValue}°', unit:"F",
 				backgroundColors:[
-					[value: 31, color: "#153591"],
-					[value: 44, color: "#1e9cbb"],
-					[value: 59, color: "#90d2a7"],
-					[value: 74, color: "#44b621"],
-					[value: 84, color: "#f1d801"],
-					[value: 95, color: "#d04e00"],
-					[value: 96, color: "#bc2323"]
-				]
+        [value: 0, color: "#153591"],
+				[value: 7, color: "#1e9cbb"],
+				[value: 15, color: "#90d2a7"],
+				[value: 23, color: "#44b621"],
+				[value: 29, color: "#f1d801"],
+				[value: 35, color: "#d04e00"],
+				[value: 36, color: "#bc2323"],
+									// fahrenheit range
+				[value: 37, color: "#153591"],
+				[value: 44, color: "#1e9cbb"],
+				[value: 59, color: "#90d2a7"],
+				[value: 74, color: "#44b621"],
+				[value: 84, color: "#f1d801"],
+				[value: 95, color: "#d04e00"],
+				[value: 96, color: "#bc2323"]
+        ]
 			)
 		}
 		standardTile("mode", "device.thermostatMode", inactiveLabel: false, decoration: "flat") {
-			state "off", label:'${name}', action:"thermostat.setThermostatMode"
-			state "cool", label:'${name}', action:"thermostat.setThermostatMode"
-			state "heat", label:'${name}', action:"thermostat.setThermostatMode"
-			state "emergencyHeat", label:'${name}', action:"thermostat.setThermostatMode"
+			state "off",   action:"thermostat.setThermostatMode", icon:"st.thermostat.heating-cooling-off"
+			state "cool",  action:"thermostat.setThermostatMode", icon:"st.thermostat.cool"
+			state "heat",  action:"thermostat.setThermostatMode", icon:"st.thermostat.heat"
+			state "emergencyHeat", label:'${name}', action:"thermostat.setThermostatMode", icon:"st.thermostat.auto"
 		}
 		standardTile("fanMode", "device.thermostatFanMode", inactiveLabel: false, decoration: "flat") {
 			state "fanAuto", label:'${name}', action:"thermostat.setThermostatFanMode"
 			state "fanOn", label:'${name}', action:"thermostat.setThermostatFanMode"
 		}
+
+    standardTile("hvacStatus", "modeStatus", inactiveLabel: false, decoration: "flat") {
+				state "Resting",  label: 'Resting'
+				state "Heating",  icon:"st.thermostat.heating"
+				state "Cooling",  icon:"st.thermostat.cooling"
+		}
+
+
 		controlTile("heatSliderControl", "device.heatingSetpoint", "slider", height: 1, width: 2, inactiveLabel: false) {
 			state "setHeatingSetpoint", action:"thermostat.setHeatingSetpoint", backgroundColor:"#d04e00"
 		}
@@ -64,7 +82,7 @@ metadata {
 			state "configure", label:'', action:"configuration.configure", icon:"st.secondary.configure"
 		}
 		main "temperature"
-		details(["temperature", "mode", "fanMode", "heatSliderControl", "heatingSetpoint", "coolSliderControl", "coolingSetpoint", "refresh", "configure"])
+		details(["temperature", "mode", "hvacStatus", "heatSliderControl", "heatingSetpoint", "coolSliderControl", "coolingSetpoint", "refresh", "configure"])
 	}
 }
 
@@ -96,7 +114,11 @@ def parse(String description) {
 			log.debug "FAN MODE"
 			map.name = "thermostatFanMode"
 			map.value = getFanModeMap()[descMap.value]
-		}
+		} else if (descMap.cluster == "0201" && descMap.attrId == "0029") {
+      log.debug "Mode Status"
+      map.name = "modeStatus"
+      map.value = getModeStatus(descMap.value)
+    }
 	}
 
 	def result = null
@@ -117,14 +139,28 @@ def parseDescriptionAsMap(description) {
 def getModeMap() { [
 	"00":"off",
 	"03":"cool",
-	"04":"heat",
-	"05":"emergencyHeat"
+	"04":"heat"
 ]}
 
 def getFanModeMap() { [
 	"04":"fanOn",
 	"05":"fanAuto"
 ]}
+
+def getModeStatus(value)
+{
+	String[] m = [ "Heating", "Cooling", "Fan", "Heat2", "Cool2", "Fan2", "Fan3"]
+	String desc = 'Resting'
+		value = Integer.parseInt(value, 16)
+
+		// only check for 1-stage  for A1730
+	for ( i in 0..2 ) {
+		if (value & 1 << i)
+			desc = m[i]
+	}
+
+	desc
+}
 
 def refresh()
 {
@@ -134,7 +170,7 @@ def refresh()
 		"st rattr 0x${device.deviceNetworkId} 1 0x201 0x11", "delay 200",
   		"st rattr 0x${device.deviceNetworkId} 1 0x201 0x12", "delay 200",
 		"st rattr 0x${device.deviceNetworkId} 1 0x201 0x1C", "delay 200",
-		"st rattr 0x${device.deviceNetworkId} 1 0x202 0"
+		"st rattr 0x${device.deviceNetworkId} 1 0x201 0x29"
 	]
 }
 // Leaving out for now as its killing the batteries.
@@ -146,7 +182,7 @@ def refresh()
 def getTemperature(value) {
 	def celsius = Integer.parseInt(value, 16) / 100
 	if(getTemperatureScale() == "C"){
-		return celsius
+		return celsius as Integer
 	} else {
 		return celsiusToFahrenheit(celsius) as Integer
 	}
