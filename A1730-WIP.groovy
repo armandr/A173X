3b6eba4df94c4cd06860f7230a709058efa9f85a
armandr <Armand.raz@gmail.com>
2015-04-14 15:27:04 -0400

    added reporting

--- A1730-WIP.groovy
+++ A1730-WIP.groovy
@@ -13,15 +13,24 @@ metadata {
 		capability "Configuration"
 		capability "Refresh"
 		capability "Sensor"
+        capability "Polling"
 
     attribute "modeStatus", "string"
 
-				fingerprint profileId: "0104", inClusters: "0000,0003,0004,0005,0201,0204,0B05", outClusters: "000A, 0019"
+	command "setThermostatTime"
+
+	fingerprint profileId: "0104", inClusters: "0000,0003,0004,0005,0201,0204,0B05", outClusters: "000A, 0019"
 
 	}
 
 	// simulator metadata
 	simulator { }
+  // pref
+     preferences {
+ 		input "tempOffset", "number", title: "Temperature Offset", description: "Adjust temperature by this many degrees", range: "*..*", displayDuringSetup: false
+        input ("sync_clock", "boolean", title: "Synchronize Thermostat Clock Automatically?", options: ["Yes","No"])
+
+ 	}
 
 	tiles {
 		valueTile("temperature", "device.temperature", width: 2, height: 2) {
@@ -173,11 +182,11 @@ def refresh()
 		"st rattr 0x${device.deviceNetworkId} 1 0x201 0x29"
 	]
 }
-// Leaving out for now as its killing the batteries.
-//def poll() {
-//	log.debug "Executing 'poll'"
-//	refresh()
-//}
+
+def poll() {
+	log.debug "Executing 'poll'"
+	refresh()
+}
 
 def getTemperature(value) {
 	def celsius = Integer.parseInt(value, 16) / 100
@@ -204,7 +213,10 @@ def setCoolingSetpoint(degrees) {
 	log.debug "setCoolingSetpoint({$degreesInteger} ${temperatureScale})"
 	sendEvent("name":"coolingSetpoint", "value":degreesInteger)
 	def celsius = (getTemperatureScale() == "C") ? degreesInteger : (fahrenheitToCelsius(degreesInteger) as Double).round(2)
-	"st wattr 0x${device.deviceNetworkId} 1 0x201 0x11 0x29 {" + hex(celsius*100) + "}"
+	[
+    "st wattr 0x${device.deviceNetworkId} 1 0x201 0x11 0x29 {" + hex(celsius*100) + "}",
+    "info"
+  ] //+ setThermostatTime()
 }
 
 def modes() {
@@ -293,13 +305,68 @@ def fanAuto() {
 	"st wattr 0x${device.deviceNetworkId} 1 0x202 0 0x30 {05}"
 }
 
+def updated()
+{
+    log.debug "updated, scheduling set time"
+    // run every two minutes
+    //schedule("0 0/2 * * * ?", setThermostatTime)
+    //unschedule()
+    //log.trace "commands: "  + device
+}
+
+def setThermostatTime()
+{
+
+  log.debug "setting time called.  Sending to: ${device.deviceNetworkId}"
+
+  if ((settings.sync_clock ?: "No") == "No")
+    {
+      log.debug "sync time is disabled, leaving"
+      return []
+    }
+
+  //Calendar calendar = Calendar.getInstance();
+  //calendar.set(Calendar.MILLISECOND, 0); // Clear the millis part. Silly API.
+  //calendar.set(2000, 0, 1, 0, 0, 0); // Note that months are 0-based
+  // java zigbee epoch 946684800000
+  //
+
+  Date date = new Date();
+  String zone = location.timeZone.getRawOffset() + " DST " + location.timeZone.getDSTSavings();
+
+	log.debug "sync time for ${device.deviceNetworkId} at ${date} location: ${zone}"
+
+	long millis = date.getTime(); // Millis since Unix epoch
+  millis -= 946684800000;  // adjust for ZigBee EPOCH
+  // adjust for time zone and DST offset
+	millis += location.timeZone.getRawOffset() + location.timeZone.getDSTSavings();
+	//convert to seconds
+	millis /= 1000;
+
+	// print to a string for hex capture
+  String s = String.format("%08X", millis);
+	// hex capture for message format
+  String data = " " + s.substring(6, 8) + " " + s.substring(4, 6) + " " + s.substring(2, 4)+ " " + s.substring(0, 2);
+
+	log.trace "time data: ${data}"
+	[
+  "raw 0x201 {04 21 11 00 02 0f 00 23 ${data} }", "delay 100",
+  "send 0x${device.deviceNetworkId} 1 1", "delay 200",
+  ]
+}
+
+
 def configure() {
 
 	log.debug "binding to Thermostat and Fan Control cluster"
 	[
 		"zdo bind 0x${device.deviceNetworkId} 1 1 0x201 {${device.zigbeeId}} {}", "delay 200",
-		"zdo bind 0x${device.deviceNetworkId} 1 1 0x202 {${device.zigbeeId}} {}"
-	]
+		"zdo bind 0x${device.deviceNetworkId} 1 1 0x204 {${device.zigbeeId}} {}", "delay 200",
+        //"zcl global send-me-a-report [cluster:2] [attributeId:2] [dataType:1] [minReportTime:2] [maxReport-Time:2] [reportableChange:-1]"
+  		"zcl global send-me-a-report 0x201 0x0000 0x29 20 30 {01}", "delay 100",
+  		"send 0x${device.deviceNetworkId} 1 1", "delay 200",
+
+]
 }
 
 private hex(value) {
